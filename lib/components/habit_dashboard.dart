dda88aec76729951f388837a0c1ab4db17aaf022
Evellyn Lima <evellynlrl@hotmail.com>
2021-11-03 20:46:21 -0300

    Add CalendarDay as class

--- lib/components/habit_dashboard.dart
+++ lib/components/habit_dashboard.dart
@@ -18,15 +18,6 @@ class HabitDashboard extends StatelessWidget {
   }
 }
 
-class Today extends StatelessWidget {
-  const Today({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Text('jsijasdij');
-  }
-}
-
 class Calendar extends StatelessWidget {
   const Calendar({Key? key}) : super(key: key);
 
@@ -38,101 +29,57 @@ class Calendar extends StatelessWidget {
       focusedDay: DateTime.now(),
       calendarBuilders: CalendarBuilders(
         todayBuilder: (context, date, _) {
-          print(date);
-          // final string text = date
-          return Center(
-            child: Container(
-              alignment: Alignment.center,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(date.day.toString()),
-                  SizedBox(
-                    width: 4,
-                  ),
-                  Container(
-                      height: 20,
-                      width: 20,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(4),
-                          border: Border.all(color: Colors.black, width: 2)))
-                ],
-              ),
-            ),
+          return CalendarDay(
+            day: date.day.toString(),
+            isToday: true,
           );
         },
         defaultBuilder: (context, date, _) {
-          return Center(
-            child: Container(
-              alignment: Alignment.center,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(date.day.toString()),
-                  SizedBox(
-                    width: 4,
-                  ),
-                  Container(
-                      height: 20,
-                      width: 20,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(4),
-                          border: Border.all(color: Colors.black, width: 2)))
-                ],
-              ),
-            ),
-          );
+          return CalendarDay(day: date.day.toString());
         },
       ),
     );
   }
-  // Widget build(BuildContext context) {
-  //   return Wrap(
-  //     direction: Axis.vertical,
-  //     spacing: 10,
-  //     children: <Widget>[
-  //       Wrap(
-  //         spacing: 20,
-  //         children: const [
-  //           Text('Dom'),
-  //           Text('Seg'),
-  //           Text('Ter'),
-  //           Text('Qua'),
-  //           Text('Qui'),
-  //           Text('Sex'),
-  //           Text('Sáb'),
-  //         ],
-  //       ),
-  //       for (int weeks = 0; weeks <= 4; weeks++) WeekCalendar()
-  //     ],
-  //   );
-  // }
 }
 
-// class WeekCalendar extends StatelessWidget {
-//   const WeekCalendar({Key? key}) : super(key: key);
+class CalendarDay extends StatelessWidget {
+  final String day;
+  final bool isToday;
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Row(children: <Widget>[
-//       Wrap(
-//         spacing: 18,
-//         children: [for (int days = 0; days <= 7; days++) CalendarDay()],
-//       )
-//     ]);
-//   }
-// }
+  const CalendarDay({
+    Key? key,
+    this.day = '',
+    this.isToday = false,
+  }) : super(key: key);
 
-// class CalendarDay extends StatelessWidget {
-//   const CalendarDay({Key? key}) : super(key: key);
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Container(
-//         height: 20,
-//         width: 20,
-//         decoration: BoxDecoration(
-//             borderRadius: BorderRadius.circular(4),
-//             border: Border.all(color: Colors.black, width: 2)));
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        alignment: Alignment.center,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              day.toString(),
+              style: TextStyle(
+                  color: isToday ? Colors.red : Colors.black,
+                  fontWeight: isToday ? FontWeight.bold : FontWeight.normal),
+            ),
+            SizedBox(
+              width: 4,
+            ),
+            Container(
+                height: 20,
+                width: 20,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(4),
+                    border: isToday
+                        ? Border.all(color: Colors.red, width: 2)
+                        : Border.all(color: Colors.black, width: 2)))
+          ],
+        ),
+      ),
+    );
+  }
+}
